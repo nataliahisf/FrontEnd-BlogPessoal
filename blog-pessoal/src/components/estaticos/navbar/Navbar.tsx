0bb05f996f3f509191daefac019bfef44735c4e5
Natalia <nataliahisf@gmail.com>
2022-02-16 15:56:51 -0300

    Menu, Rodapé, home através do Material Ui

--- blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
+++ blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
@@ -1,9 +1,46 @@
-import React from "react";
-
-function Navbar () {
+import React from 'react';
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+function Navbar() {
     return (
         <>
-        
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box style={{ cursor: "pointer" }} >
+                        <Typography variant="h5" color="inherit">
+                            BLOG PESSOAL
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Inicial
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Postagens
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Temas
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Cadastrar tema
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Sair
+                            </Typography>
+                        </Box>
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
         </>
     )
 }
--- blog-pessoal/src/paginas/home/Home.tsx
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -1,23 +1,29 @@
-import React from "react";
-import {Button ,Box, Paper} from "@material-ui/core";
-import "./Home.css";
+import React from 'react';
+import {Typography, Box, Grid, Button} from '@material-ui/core';
+import './Home.css';
 
-function Home(){
+function Home() {
     return (
-     <>
-        <Paper>
-            <Box p={2}>
-                <Box display="flex" justifyContent="center">
-                    <h1>Título</h1>
-                </Box>
-                <img src="https://i.imgur.com/bJriqYs.jpeg" alt="Flexbox na pratica" style={{width: "100%", height: "100%"}}/>
-                <Box display="flex" justifyContent="center" p={2}>
-                    <Button variant="contained" color="primary">Texto 1</Button>
-                    <Button variant="contained" color="secondary">Texto 2</Button>
-                </Box>
-            </Box>
-        </Paper>
-     </>
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#FFDAB9" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "#363636", fontWeight: "bold" }}>Seja muito bem vinde!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "#363636", fontWeight: "bold" }}>Conte suas dificuldades com Front-End de hoje</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "#363636", backgroundColor: "#D2691E", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/49PNUTS.png" alt="" width="400px" height="400px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+        </>
     );
 }
 
