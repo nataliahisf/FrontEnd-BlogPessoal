b623cb424ad152632100e8358922725e312734dc
Natalia <nataliahisf@gmail.com>
2022-02-23 07:03:16 -0300

    Criado tab postagem com uma postagem cadastrada

--- blog-pessoal/src/components/postagens/listapostagem/ListaPostagem.tsx
+++ blog-pessoal/src/components/postagens/listapostagem/ListaPostagem.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
-import './ListaTema.css';
+import './ListaPostagem.css';
 
-function ListaTema() {
+function ListaPostagem() {
 
   return (
     <>
@@ -11,16 +11,22 @@ function ListaTema() {
         <Card variant="outlined">
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
-              Tema
+              Postagens
             </Typography>
             <Typography variant="h5" component="h2">
-              Minha descrição
+              Título
+            </Typography>
+            <Typography variant="body2" component="p">
+              Texto da Postagem
+            </Typography>
+            <Typography variant="body2" component="p">
+              Tema
             </Typography>
           </CardContent>
           <CardActions>
-            <Box display="flex" justifyContent="center" mb={1.5} >
+            <Box display="flex" justifyContent="center" mb={1.5}>
 
-              <Link to="" className="text-decorator-none">
+              <Link to="" className="text-decorator-none" >
                 <Box mx={1}>
                   <Button variant="contained" className="marginLeft" size='small' color="primary" >
                     atualizar
@@ -38,9 +44,7 @@ function ListaTema() {
           </CardActions>
         </Card>
       </Box>
-    </>
-  );
+    </>)
 }
 
-
-export default ListaTema;
+export default ListaPostagem;
