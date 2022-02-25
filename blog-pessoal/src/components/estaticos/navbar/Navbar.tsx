b7ac63393ffa41e0b0a2ca344bef6a0b479d9a87
Natalia <nataliahisf@gmail.com>
2022-02-25 02:30:07 -0300

    Post Put e Delete da model de Tema e Postagem versao 1

--- blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
+++ blog-pessoal/src/components/estaticos/navbar/Navbar.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
 import { Link } from "react-router-dom";
+import {useHistory} from "react-router-dom";
+import useLocalStorage from 'react-use-localstorage';
 import "./Navbar.css"
 
 function Navbar() {
-    return (
+    const [token, setToken] = useLocalStorage("token");
+    let history = useHistory();
+
+    function goLogout(){
+        setToken("")
+        alert("Usuário deslogado")
+        history.push("/login")
+    }
+    return ( 
         <>
             <AppBar position="static">
                 <Toolbar variant="dense" className='backgroundColor'>
@@ -15,37 +25,43 @@ function Navbar() {
                     </Box>
 
                     <Box display="flex" justifyContent="start">
+                        <Link to="/home" className="text-decorator-none">
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                Inicial
+                                Inicio
                             </Typography>
                         </Box>
+                        </Link>
 
+                        <Link to="posts" className="text-decorator-none">
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Postagens
                             </Typography>
                         </Box>
+                        </Link>
 
+                        <Link to="temas" className="text-decorator-none">
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 Temas
                             </Typography>
                         </Box>
+                        </Link>
 
+                        <Link to="/formularioTema" className="text-decorator-none">
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit" >
                                 Cadastrar tema
                             </Typography>
                         </Box>
+                        </Link>
 
-                        <Link to="/login" className="text-decorator-none">
-                            <Box mx={1} className='cursor'>
+                            <Box mx={1} className='cursor' onClick={goLogout}>
                                 <Typography variant="h6" color="inherit">
                                     Sair
                                 </Typography>
                          </Box>
-                        </Link>
 
                 </Box>
 
